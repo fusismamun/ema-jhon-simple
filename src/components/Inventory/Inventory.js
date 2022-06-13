49473623831668b6811126bd5b1440662f43e216
unknown <abdullahmamunbd46@gmail.com>
2022-06-13 16:15:23 +0600

    update

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h1>This is Inventory</h1>
+            <h1>This is Inventory page</h1>
         </div>
     );
 };
